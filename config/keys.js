23c47e8461edb32607380f102094d496a01912ee
tsimnujhawj <tsimnuj.hawj@gmail.com>
2018-10-16 18:54:44 -0500

    fix

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,10 @@
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./keys_prod');
-} else {
-  module.exports = require('./keys_dev');
-}
+// if (process.env.NODE_ENV === 'production') {
+//   module.exports = require('./keys_prod');
+// } else {
+//   module.exports = require('./keys_dev');
+// }
+
+module.exports = {
+  mongoURI: "mongodb://user1:user1user1@ds027345.mlab.com:27345/devconnector",
+  secretOrKey: "jd9cmrJDCKF4nflJDHFNInvol"
+};
